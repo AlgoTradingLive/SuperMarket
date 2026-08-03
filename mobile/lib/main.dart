1b2bc6ef1ee2ebc019b13c2b35d2d435b2b4d20e
AlgoTradingLive <smkinpune@gmail.com>
2026-08-03 14:36:40 +0530

    Add files via upload

---
+++ mobile/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const QuickMartApp());
+}
+
+class QuickMartApp extends StatelessWidget {
+  const QuickMartApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'QuickMart',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color(0xFF0B6E4F),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF0B6E4F),
+        ),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
